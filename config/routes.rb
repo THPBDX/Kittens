bf43ea8ef90e1a600c006c40b4560a3d33371fd8
cris-dev <cristiano.ferreira.dev@gmail.com>
2019-03-06 13:12:24 +0100

    kittens index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root "home#index"
+  get 'kittens/index'
+  root "kittens#index"
   devise_for :users
-  resources :home 
+  resources :home
+  resources :kittens
 end
