cd251b45d82cc258dda497728390ba842d9cdeb5
Morgane <twixigame9@gmail.com>
2019-03-06 15:50:14 +0100

    Add password mailer V4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'kittens-eshop.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
