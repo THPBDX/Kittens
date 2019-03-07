4757eca7e4a16a3a7ec11adc1636d7e3b7849d28
cris-dev <cristiano.ferreira.dev@gmail.com>
2019-03-07 17:25:18 +0100

    init cart (model + controller)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,5 +66,6 @@ Rails.application.configure do
   # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :mailjet
+  #config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.delivery_method = :letter_opener
 end
